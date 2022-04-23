bbf1272def016aaf6fb10b2b60c7a37fb7518c19
mJunaid15 <junaidjjd44@gmail.com>
2022-04-23 18:49:20 +0500

    update

---
+++ mernbackend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // content: ['*'],
+  content: [
+    './templates/views/**/*.{hbs,html,js}', 
+    './templates/partials/**/*.{hbs,html,js}', 
+
+    './src/**/*.js', 
+
+    
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
